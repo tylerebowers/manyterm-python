39c155cce6947652fe32de083bd474cc35306251
Tyler Bowers <tylerebowers@gmail.com>
2024-04-12 22:44:43 -0400

    Made Text window scalable, added some customization, and improved closing.

--- multiTerminal.py
+++ multiTerminal.py
@@ -1,43 +1,68 @@
 from time import sleep
 import tkinter as tk
-from tkinter.scrolledtext import ScrolledText
+from tkinter import ttk
 import threading
 
 
 class terminal(threading.Thread):
 
-    def __init__(self, title="Terminal", height=20, width=100, font=("Courier New", "12")):
+    def __init__(self, title="Terminal", height=20, width=100, font=("Courier New", "12"), allowClosing=True, ignoreClosedPrints=True):
+        #params
         self.title = title
         self.height = height
         self.width = width
         self.font = font
-        self.root = None
-        self.st = None
+        self.allowClosing = allowClosing
+        self.ignoreClosedPrints = ignoreClosedPrints
+        #internal variables
+        self._root = None  # main window
+        self._tb = None  # text box
+        self.isOpen = False  # if the window is open
+        self._safeExit = False  # if the window is safe to exit
         threading.Thread.__init__(self, daemon=True)  # kill child thread on main thread exit
         self.start()
         sleep(0.5)  # wait for window to open
 
     def close(self):
-        if self.root is not None:
-            self.root.quit()
-            self.root.update()
-            self.root = None
-            self.st = None
+        if self.isOpen and self._safeExit:  # safeExit is True is no operations are being done (i.e. print)
+            self.isOpen = False
+            self._root.quit()
+            self._root.update()
+            self._root = None
 
     def run(self):
-        self.root = tk.Tk()
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-        self.root.title(self.title)
-        self.st = ScrolledText(self.root, height=self.height, width=self.width, wrap="none", font=self.font)
-        self.st.pack()
-        self.root.mainloop()
+        self._root = tk.Tk()
+        if self.allowClosing: self._root.protocol("WM_DELETE_WINDOW", self.close)
+        else: self._root.protocol("WM_DELETE_WINDOW", lambda: None)
+        self._root.title(self.title)
+        self._tb = tk.Text(self._root, height=self.height, width=self.width, wrap="none", font=self.font)
+        self._tb.pack(side="left", fill="both", expand=True)
+        self._tb.configure(state="disabled")
+        sizegrip = ttk.Sizegrip(self._tb)
+        sizegrip.configure(cursor="sizing")
+        sizegrip.bind("<1>", self._resize_start)
+        sizegrip.bind("<B1-Motion>", self._resize_update)
+        self.isOpen = True
+        self._safeExit = True
+        self._root.mainloop()
 
-    def print(self, toPrint):
-        if self.root is None or self.st is None:
-            raise Exception(f"Terminal \"{self.title}\" is closed")
-            #print(f"Terminal {self.title} is closed")
+    def print(self, toPrint, end="\n"):
+        if self._root is None or not self.isOpen:
+            if not self.ignoreClosedPrints: return print(f"Terminal \"{self.title}\" is closed")
+            #if not self.ignoreClosedPrints: raise Exception(f"Terminal \"{self.title}\" is closed")
         else:
-            self.st.configure(state="normal")
-            self.st.see("end")  # show last line printed (could also put this under "insert" but there would be a gap)
-            self.st.insert("end", toPrint)
-            self.st.configure(state="disabled")
+            self._safeExit = False  # needed so that we don't kill mainloop while printing
+            self._tb.configure(state="normal")
+            self._tb.see("end")
+            self._tb.insert("end", f"{toPrint}{end}")
+            self._tb.configure(state="disabled")
+            self._safeExit = True
+
+    def _resize_start(self, event):
+        self._x = event.x
+        self._y = event.y
+
+    def _resize_update(self, event):
+        delta_x = event.x - self._x
+        delta_y = event.y - self._y
+        self._tb.place_configure(width=self._tb.winfo_width() + delta_x, height=self._tb.winfo_height() + delta_y)
--- test.py
+++ test.py
@@ -7,23 +7,23 @@ t1 = terminal()
 t2 = terminal(title="Customized Terminal", height=30, width=80, font=("Arial", "20"))
 
 #use the new terminals
-for i in range(11):
+for i in range(51):
     print(f"Main terminal: {i}")
-    t1.print(f"Terminal 1: {i}\n")
-    t2.print(f"Terminal 2: {i}\n")
+    t1.print(f"Terminal 1: {i}")
+    t2.print(f"Terminal 2: {i}")
     sleep(0.1)
 
-t1.print("This is Terminal 1\n")
-t2.print("This is Terminal 2\n")
+t1.print("This is Terminal 1")
+t2.print("This is Terminal 2")
 t2.print("This is a long line to show that the text will not wrap around to the next line. Here is some more text so that I can actually make the entire length.")
 print("This is the main terminal")
 
-#close the terminals (this is not necessary if the program is to be terminated)
-print("Closing terminals in 5 seconds")
-sleep(5)
+# close terminal 1
+print("Closing terminal 1 in 10 seconds.")
+sleep(10)
 t1.close()
-t2.close()
 
-print("Exiting in 5 seconds")
-sleep(5)
+# close terminal 2 on exit
+print("Exiting in 10 seconds, terminal 2 will close when the main program exits.")
+sleep(10)
 quit()
